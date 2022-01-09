94e95320c942f389e90513ffd8a4d83d402f18f7
Omar Osman <45637656+omariosman@users.noreply.github.com>
2022-01-09 22:16:18 +0200

    Update entropy_calculator.py

--- entropy_calculator.py
+++ entropy_calculator.py
@@ -1,8 +1,27 @@
 import math 
 
-x = int(input("First: "))
-y = int(input("Second: "))
-sum = x + y
-entropy = (-(x/sum) * math.log(x/sum,2)) - ((y/sum) * math.log(y/sum,2))
 
-print(entropy)
+main_input = ""
+while(True):
+    try:
+        main_input = input("First: ")
+        x = int(main_input)
+        y = int(input("Second: "))
+        
+    except:
+        if (main_input == "q"):
+            print("Byee")
+            exit(0)
+        print("Sorry, the input is invalid") 
+    else:
+        sum = x + y
+        if (x == 0 or y == 0):
+            entropy = 0
+            print("Entropy: {}".format(entropy))
+            continue
+        try:
+            entropy = (-(x/sum) * math.log(x/sum,2)) - ((y/sum) * math.log(y/sum,2))
+            print("Entropy: {}".format(entropy))
+        except:
+            print("Something went wrong while calculating the entropy") 
+
